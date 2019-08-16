fe2255a8cad2372d43c62cdc4d95599fc977a7d3
Ubuntu <ubuntu@ip-172-31-17-91.ec2.internal>
2019-08-16 12:28:50 +0000

    styled the table and added drop down menu to manage tasks

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_pymongo import PyMongo
 import bson
 #from bson.objectid import ObjectId
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 mongo = PyMongo(app)
 
@@ -19,7 +19,7 @@ def test():
 def render_tasks_table():
     return render_template('tasks_table.html', tasks=mongo.db.tasks.find())
 
-    
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
