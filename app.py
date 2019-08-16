90af1586e90b060af78b482d0ff470538ebea8f2
Ubuntu <ubuntu@ip-172-31-17-91.ec2.internal>
2019-08-16 18:08:39 +0000

    functionality to edit tasks

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 import bson
-#from bson.objectid import ObjectId
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -33,6 +33,37 @@ def insert_new_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('render_tasks_table'))
+
+
+@app.route('/edit_task/<task_id>')
+def render_edit_task(task_id):
+    task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+    return render_template('edit_task.html',
+                           task=task,
+                           categories=mongo.db.task_category.find(),
+                           statuses=mongo.db.task_status.find(),
+                           importances=mongo.db.task_importance.find())
+
+
+@app.route('/update_task/<task_id>', methods=['POST',])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+    print(type(task), task)
+    tasks.update({'_id': ObjectId(task_id)},
+                 {
+                    'name':request.form.get('name'),
+                    'category':request.form.get('category'),
+                    'description': request.form.get('description'),
+                    'status':request.form.get('status'),
+                    'importance':request.form.get('importance'),
+                    'due_date': request.form.get('due_date'),
+                    'start_timestamp': task.get('start_timestamp'),
+                    'end_timestamp': task.get('end_timestamp'),
+                    'notes': task.get('notes'),
+                   })
+    return redirect(url_for('render_tasks_table'))
+
 
 
 if __name__ == '__main__':
